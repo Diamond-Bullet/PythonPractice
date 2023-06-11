4e67230c2856402686e8b722d534e7674c228279
szq-123 <2863301875@qq.com>
2023-06-11 20:36:22 +0800

    add common libs

--- python/libs.py
+++ python/libs.py
@@ -1,6 +1,15 @@
+import bisect
 import collections
 import queue
 
-q = queue.SimpleQueue() # queue
+# queue
+q = queue.SimpleQueue()
 
-cnt = collections.Counter([1,2,3,4,3,33,2,2,3,3,2,2,2,2]) # return a dict
+# Counter. return a dict
+cnt = collections.Counter([1, 2, 3, 4, 3, 33, 2, 2, 3, 3, 2, 2, 2, 2])
+
+# binary search
+# bisect_left return the leftmost index
+index = bisect.bisect_left([1, 2, 3, 3, 4, 5], 3)
+# insert the item in the leftmost position of arr.
+bisect.insort_left([1, 2, 3, 4, 5, 6, 6, 7], 6)
