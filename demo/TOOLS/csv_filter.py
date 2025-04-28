2c1a2875c6fab65ac1ebe3a1dc6831ee8c847290
royalsong(宋彰清) <royalsong@futunn.com>
2025-04-28 14:29:49 +0800

    migration

--- demo/TOOLS/csv_filter.py
+++ demo/TOOLS/csv_filter.py
@@ -1,12 +1,16 @@
 # Open the file in read mode ('r')
-with open('filename.csv', 'r') as file:
-    # Iterate over each line in the file
-    counter = {}
-    for i, line in enumerate(file):
-        columns = line.split(',')
-        if len(columns) < 13:
-            continue
+try:
+    with open('filename.csv', 'r') as file:
+        # Iterate over each line in the file
+        counter = {}
+        for i, line in enumerate(file):
+            columns = line.split(',')
+            if len(columns) < 13:
+                continue
 
-        counter[columns[4]] = [columns[7], columns[9]]
-    print(counter)
+            counter[columns[4]] = [columns[7], columns[9]]
+        print(counter)
+except OSError as err:
+    print("Open file failed: {0}".format(err))
+
 # TODO use multithreading to speed up the process
